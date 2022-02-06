47ac798540f1fda68347c226012bf593b1199ff2
Jan Sembdner <jan.sembdner@gmail.com>
2022-02-06 22:30:59 +0100

    generated package

---
+++ SwiftyInjection/README.md
@@ -0,0 +1,3 @@
+# SwiftyInjection
+
+A description of this package.
---
+++ SwiftyInjection/Sources/SwiftyInjection/SwiftyInjection.swift
@@ -0,0 +1,6 @@
+public struct SwiftyInjection {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
